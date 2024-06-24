a260303487bf122d0a15b144c962653af57362ec
0Neocortexx <jonathanemmanueldeoliveira2003@gmail.com>
2024-06-24 20:27:25 -0300

    Modelo base de conexão com banco de dados postgres

---
+++ SalesWebMvc/Migrations/20240624230308_AddProductTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SalesWebMvc.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    valor = table.Column<float>(type: "real", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
--- SalesWebMvc/Program.cs
+++ SalesWebMvc/Program.cs
@@ -1,26 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.data;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Adicionar serviços ao contêiner
 builder.Services.AddControllersWithViews();
 
 // Configurar a conexão com o banco de dados PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
-// Add services to the container.
-//builder.Services.AddControllersWithViews();
-
-// Configure the HTTP request pipeline.
+// Configurar o pipeline de solicitação HTTP
 if(!app.Environment.IsDevelopment())
 {
-    //app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
 
